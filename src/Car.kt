af4ebe8c5ea6d996519e249769c01aa69c9737db
marysaray <msaray.ms@outlook.com>
2024-04-27 22:47:28 -0700

    Created CarProject

--- src/Car.kt
+++ src/Car.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 // add a constructor by adding () after class Car
 class Car( val year: Int, val make : String, val gasCapacity : Double,
 val mpg : Double, var speed : Int) {
@@ -10,15 +13,59 @@ val mpg : Double, var speed : Int) {
     init { // brand-new car
         // set odometer
         odometer = 0.0
-        // set gasLevel
+        // set gasLevel == fill gas tank
         gasLevel = gasCapacity
         // set gasLevel
         mileageAtOilChange = 0.0
+    }
+
+    // add accelerate method
+    fun accelerate() {
+        // speed add +5 but not greater than 180
+        speed = min(speed + 5, 180)
+    }
+
+    // add brake method
+    fun brake() {
+        speed = max(speed - 5, 0)
     }
 
     // add toString method by using override
     override fun toString() : String {
         // return make, year, speed
         return make + ":" + year + ":" + speed
+    }
+    // add drive method
+    fun drive(hours : Double) {
+        // add a variable
+        val distance : Double = hours * speed
+        val fuelConsumed: Double = hours * speed / mpg
+        // not enough gas to drive anymore
+        if (fuelConsumed > gasLevel) {
+            // add to odometer as far as we can go
+            odometer += gasLevel * mpg // total gas = gallons is number of gallons convert to distance: miles = gallon * mpg
+            gasLevel = 0.0
+        }
+        else {
+            // drive a certain amount of hours * speed (certain distance)
+            odometer += distance
+            // lose gas: time driven, speed driven, mpg
+            gasLevel -= fuelConsumed
+        }
+    }
+    // add gas method
+    fun addGas(gallons: Double) {
+        gasLevel += min(gasCapacity, gasLevel + gallons)
+    }
+
+    // how often to do an oil change depends on the odometer
+    fun needOilChange() : Boolean {
+        // 5,000 since oilChange
+        return (odometer - mileageAtOilChange > 5000)
+    }
+
+    // change the oil
+    fun changeOil() {
+        mileageAtOilChange = odometer
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,12 +2,51 @@ fun main() {
     //  instantiate a car
     val myMustang = Car(1964, "Ford", gasCapacity = 12.0, mpg = 20.0, speed = 96)
 
+    // Programming Assignment 4
+    myMustang.speed = 100
+    myMustang.drive(40.0)
+    println("odometer: " + myMustang.odometer) // question: odometer: 4400.0 (prints == 240.0)
+    println("needOilChange: " + myMustang.needOilChange()) // the same: needOilChange: false
+    println("mileageAtOilChange: " + myMustang.mileageAtOilChange) // the same: mileageAtOilChange: 0.0
+    myMustang.drive(40.0)
+    println("odometer: " + myMustang.odometer) // question: odometer 8400.0 (prints == 240.0)
+    println("needOilChange: " + myMustang.needOilChange()) // question: needOilChange: true (prints == false)
+    myMustang.changeOil()
+    println("needOilChange: " + myMustang.needOilChange()) // the same: needOilChange: false
+    println("mileageAtOilChange: " + myMustang.mileageAtOilChange) // question: mileageAtOilChange: 8400.0 (prints == 240.0)
+
+    // In class Exercise
     println(myMustang) // car class doesn't have a toString method: display data type & address location
     println(myMustang.gasLevel)
-    // myMustang.drive(100) // need drive method
+    myMustang.drive(100.0) // need drive method
     println(myMustang.gasLevel)
     println(myMustang.odometer)
-    // myMustang.addGas(3) // need addGas method
+    myMustang.addGas(3.0) // need addGas method
     println(myMustang.gasLevel)
     println(myMustang.year)
+
+    // speed up to the max
+    for (i in 0..20) {
+        myMustang.accelerate()
+        println(myMustang.speed)
+    }
+    println("Hit the brakes!!!")
+
+    // brake to zero
+    for (i in 0..40) {
+        myMustang.brake()
+        println(myMustang.speed)
+    }
+
+    // Programming Assignment 4
+    for (i in 0.. 30) {
+        // put gas in
+        myMustang.addGas(12.0)
+        myMustang.drive(200.0)
+        println(myMustang.odometer)
+        if (myMustang.needOilChange()) {
+            myMustang.changeOil()
+            println("Oil change: " + myMustang.mileageAtOilChange)
+        }
+    }
 }
