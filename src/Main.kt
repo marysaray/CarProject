81155665cde85d0bbed4146f3e8e96c71aa2234f
marysaray <msaray.ms@outlook.com>
2024-04-27 21:00:02 -0700

    Created CarProject

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
+    val myMustang = Car(1964, "Ford", gasCapacity = 12.0, mpg = 20.0, speed = 96)
 
+    println(myMustang)
+    println(myMustang.gasLevel)
+    myMustang.drive(100)
+    println(myMustang.gasLevel)
+    println(myMustang.odometer)
+    // myMustang.addGas(3)
+    println(myMustang.gasLevel)
+    println(myMustang.year)
 }
